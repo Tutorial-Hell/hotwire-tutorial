89e1953c611254ad5c7cd46c6464141279a6c0e3
Andrew <purplebetter1>
2025-10-03 22:19:21 -0400

    devise authentication

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "simple_form", "~> 5.1.0"
 
 gem "redis", "~> 4.2"
 
+gem "devise", "~> 4.9.0"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action -> { sleep 3 }
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!, unless: :devise_controller?
   allow_browser versions: :modern
 end
