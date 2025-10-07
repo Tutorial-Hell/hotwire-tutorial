7dd9bcf0312757b4c6690db23f705173352d53ab
Andrew <purplebetter1>
2025-10-07 00:25:31 -0400

    secure broadcast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :authenticate_user!, unless: :devise_controller?
   allow_browser versions: :modern
+
+  private
+  def current_company
+    @current_company||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
 end
