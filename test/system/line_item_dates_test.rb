9f28242f6d4ec467f09c71642fdc1533ec3182ac
Andrew <purplebetter1>
2025-11-15 19:43:11 -0500

    add quote total bar

--- test/system/line_item_dates_test.rb
+++ test/system/line_item_dates_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class LineItemDatesTest < ApplicationSystemTestCase
+  include ActionView::Helpers::NumberHelper
+
   setup do
     login_as users(:accountant)
 
@@ -21,24 +23,27 @@ class LineItemDatesTest < ApplicationSystemTestCase
   test "Updating a line item date" do
     assert_selector "h1", text: "First quote"
 
-    within id: dom_id(@line_item_date) do
+    within id: dom_id(@line_item_date, :edit) do
       click_on "Edit"
     end
 
     assert_selector "h1", text: "First quote"
+
     fill_in "Date", with: Date.current + 1.day
     click_on "Update date"
+
     assert_text I18n.l(Date.current + 1.day, format: :long)
   end
 
   test "Destroying a line item date" do
-    assert_text I18n.l(Date.current + 1.day, format: :long)
+    assert_text I18n.l(Date.current, format: :long)
 
     accept_confirm do
-      within id: dom_id(@line_item_date) do
+      within id: dom_id(@line_item_date, :edit) do
         click_on "Delete"
       end
     end
-    assert_no_text I18n.l(Date.current + 1.day, format: :long)
+    assert_no_text I18n.l(Date.current, format: :long)
+    assert_text number_to_currency(@quote.total_price)
   end
 end
